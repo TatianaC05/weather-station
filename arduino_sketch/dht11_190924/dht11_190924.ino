e3a952e63d3b2a7bc3672e7b9c7627b4c53d8e95
TatianaC05 <169114295+TatianaC05@users.noreply.github.com>
2024-10-16 20:57:36 -0500

    Add files via upload

---
+++ arduino_sketch/dht11_190924/dht11_190924.ino
@@ -0,0 +1,38 @@
+/*
+Date: 19-09-2024
+Developer: Tatiana C.
+Sketch description: Get temperature and humidity from DHT11 Sensor
+*/
+#include "DHT.h"
+#define DHTTYPE DHT11
+#define DHTPIN 5
+
+float temp = 0;
+float hum = 0;
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  // put your setup code here, to run once:
+  dht.begin();
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(1000);
+  temp = dht.readTemperature();
+  hum = dht.readHumidity();
+
+  if(isnan(temp) || isnan(hum)){
+    Serial.println("DHT 11 reading error");
+    return;
+  }
+
+  Serial.print("Temperatura actual: ");
+  Serial.println(temp);
+  Serial.print("Humedad actual: ");
+  Serial.println(hum);
+
+}
